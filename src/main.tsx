c68341bcc22f39b43e0f82fd026b462a143f338d
ionut <ionut_p_o_p@yahoo.com>
2026-02-01 17:42:54 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,6 @@
-import React from 'react';
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { TooltipProvider } from '@/components/ui/tooltip'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TooltipProvider>
-      <App />
-    </TooltipProvider>
-  </React.StrictMode>,
-)
 
 // Set dark theme by default
 document.documentElement.classList.add('light');
