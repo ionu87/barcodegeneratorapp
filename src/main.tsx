4893a3a96edd2505053fc2c0a091f13af1813324
ionut <ionut_p_o_p@yahoo.com>
2026-02-01 17:16:23 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { InfoTooltip } from '@/components/ui/info-tooltip';
+
+<div>
+  <span>Some field</span>
+  <InfoTooltip content="This explains what the field means" />
+</div>
 import App from "./App.tsx";
 import "./index.css";
 
 // Set dark theme by default
-document.documentElement.classList.add('dark');
+document.documentElement.classList.add('light');
 
 createRoot(document.getElementById("root")!).render(<App />);
